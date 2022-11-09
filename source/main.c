de7f23d13786cdc960e0e5f86c500ebe7eb4c2f4
Allan <116328571+bosuweru@users.noreply.github.com>
2022-11-09 15:37:57 -0600

    Update main.c
    Signed-off-by: Allan <116328571+bosuweru@users.noreply.github.com>

--- source/main.c
+++ source/main.c
@@ -1,3 +1,9 @@
+/* ==========================================================================
+   File: main.c  Author: Allan Boswell  Date: 21 October 2022 02∶11∶25 AM CDT
+
+   A simple "hello, world" program written in C.
+   ========================================================================== */
+
 #include <stdio.h>
 
 int main(void) {
